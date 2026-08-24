af5c5cd8189169129e0bdd98c4c842936aec510c
yarekase <ejimtpck@gmail.com>
2026-08-24 17:16:38 +0800

    修正多次初始化問題

--- backend/bin/www.js
+++ backend/bin/www.js
@@ -3,16 +3,20 @@ const { dataSource } = require('../db/data-source')
 
 async function start() {
     try {
-        await dataSource.initialize()
-        console.log('資料庫連線成功')
+        if (!dataSource.isInitialized) {
+            await dataSource.initialize()
+            console.log('資料庫連線成功')
+        }
 
-        app.listen(process.env.PORT || 8080, () => {
-            console.log(`server 跑起來了：http://localhost:${process.env.PORT || 8080}`)
-        })
+
     } catch (err) {
         console.error('資料庫連線失敗', err)
         process.exit(1)   // 沒有資料庫就不營業
     }
+
+    app.listen(process.env.PORT || 8080, () => {
+        console.log(`server 跑起來了：http://localhost:${process.env.PORT || 8080}`)
+    })
 }
 
 start()
