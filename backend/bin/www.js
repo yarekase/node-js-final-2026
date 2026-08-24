565a7cc2e899009de6dfaba224a49ba3efdb228e
yarekase <ejimtpck@gmail.com>
2026-08-24 17:10:40 +0800

    改www.js的port

--- backend/bin/www.js
+++ backend/bin/www.js
@@ -6,8 +6,8 @@ async function start() {
         await dataSource.initialize()
         console.log('資料庫連線成功')
 
-        app.listen(process.env.PORT, () => {
-            console.log(`server 跑起來了：http://localhost:${process.env.PORT}`)
+        app.listen(process.env.PORT || 8080, () => {
+            console.log(`server 跑起來了：http://localhost:${process.env.PORT || 8080}`)
         })
     } catch (err) {
         console.error('資料庫連線失敗', err)
